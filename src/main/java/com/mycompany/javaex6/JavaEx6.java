30745bf6bcb1e373d12660b4400ec26ed0d8d62f
230500226 <125464717+230500226@users.noreply.github.com>
2024-09-22 11:06:57 +0200

    init netbeans

---
+++ src/main/java/com/mycompany/javaex6/JavaEx6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaex6;
+
+/**
+ *
+ * @author ldxt460s
+ */
+public class JavaEx6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
